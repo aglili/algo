a963f3f4febd543ecccfaf17ffce1b92590e28c7
aglili <cecilselorm34@gmail.com>
2023-08-31 21:32:32 +0000

    new

---
+++ python/day1.py
@@ -0,0 +1,10 @@
+# Big O notation
+
+
+"""
+Big O notation is used to measure how running time or space requirements for
+your program increases as input grows
+"""
+
+
+#https://youtu.be/gDqQf4Ekr2A?list=PLeo1K3hjS3uu_n_a__MI_KktGTLYopZ12&t=340
